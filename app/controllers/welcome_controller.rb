2e285c4f7f9b718429bced5bebb2eb0c80a750b7
dylan <dyan2009200@gmail.com>
2022-07-27 15:33:05 +0800

    add about blog

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,14 @@
 class WelcomeController < ApplicationController
+  def home
+    #escape rails會擋
+    # render html: "<h1>HELLO</h1>".html_safe
+  @lottery =  [*1..43].sample(6)
+
+  end
+
+  def about
+  end
+
+  def blog
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/", to: "welcome#home"
+  get "/about", to: "pages#about"
+  # get "/contact", to: "pages#contact"
+  # get "/faq", to: "pages#faq"
+  get "/blog", to: "blog#index"
+
+
 end
