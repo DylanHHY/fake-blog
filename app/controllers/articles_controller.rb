13f294ab46a850110b5664b4e4517e199f6c10a7
dylan <dyan2009200@gmail.com>
2022-07-28 16:22:13 +0800

    this is my first bloggit add .

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,17 @@
 class ArticlesController < ApplicationController
   def create
+
+    @article = Article.new(article_params)
+
+    if @article.save 
+      redirect_to "/"
+    else
+      redirect_to "blog/new"
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title,:content)
   end
 end
