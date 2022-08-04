df879d4d27b05ccb1c9f5b300bfd1e34db619e74
dylan <dyan2009200@gmail.com>
2022-08-04 13:59:00 +0800

    會員登入系統

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-  
+
   private
     def record_not_found
       # redirect_to "/"
       render file: "#{Rails.root}/public/404.html", layout: false, status: 404
     end
+    
+    def current_user
+      User.find_by(id: session[:user_session])
+    end
+    
+    def user_signed_in?
+      session[:user_session] && current_user
+    end 
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,3 @@
 module UsersHelper
-  def current_user
-    User.find_by(id: session[:user_session])
-  end
-  
-  def user_signed_in?
-    session[:user_session] && current_user
-  end 
+
 end
